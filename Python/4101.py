5fcf2e0112873ffdc5854fbc2c69ec18ecb49f35
- <chris1503@naver.com>
2021-03-12 07:37:09 +0000

    Python Review 20210312

---
+++ Python/4101.py
@@ -0,0 +1,17 @@
+
+while 1:
+    
+    a, b = input().split()
+    
+    a=int(a)
+    b=int(b)
+    
+    
+    if a==0 and b==0 : 
+        break 
+    
+    if(a>b) : 
+        print("Yes")
+    
+    else : 
+        print("No")
